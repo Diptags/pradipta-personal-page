0543394cbdf53ca5dd70b359d244e37305ae1be6
diptags <rd.pradipta@gmail.com>
2025-04-29 15:00:47 +0700

    Fix email naming

--- constants/page-data/contact.js
+++ constants/page-data/contact.js
@@ -4,7 +4,7 @@ export const contactPageData = {
   title: "Let's Connect",
   intro: "Feel free to reach out if you'd like to explore new opportunities or collaborate on exciting projects. Whether you have an idea to share, insights to discuss, or just want to chat about the dynamic world of technology, I'm always open to connect. Send me a message through the form below or via social media, and let’s bring ideas to life together.",
   form_title: "Feel free to leave me a message! 👋",
-  email_title: "Hit my e-mail!: ",
+  email_title: "E-mail: ",
   alternative: {
     telegram: links.telegram,
     email: "pradipta.samiadji@gmail.com"
