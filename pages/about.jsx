1c7fc841e4a7f268aec05a75015d9594d8caf3f6
diptags <rd.pradipta@gmail.com>
2025-05-01 21:22:06 +0700

    Refactor about page

--- pages/about.jsx
+++ pages/about.jsx
@@ -12,6 +12,46 @@ const SectionHeading = ({ title, children }) => (
   </div>
 );
 
+const PhotoAndSocials = ({ story_title }) => (
+  <div className="grid grid-cols-[40%,60%] items-center mb-5">
+    <div className="flex justify-center">
+      <Image
+        src={"/images/profile.png"}
+        width={200}
+        height={200}
+        alt={"Pradipta's Profile Photo"}
+      />
+    </div>
+    <div className="flex flex-col items-center">
+      <h1 className="text-black text-xl md:text-4xl font-poppins-bold text-center">
+        {story_title}
+      </h1>
+    </div>
+  </div>
+);
+
+const Introduction = ({ name, about }) => (
+  <div>
+    <p
+      className="info text-justify"
+      dangerouslySetInnerHTML={{
+        __html: `Greetings! I'm <span class="font-bold">${name}</span>, ${about}`,
+      }}
+    />
+  </div>
+);
+
+const InterestsList = ({ interest }) => (
+  <ul>
+    {interest.list.map((item, index) => (
+      <p key={index} className="info text-justify my-2 flex gap-3">
+        <ChevronsRight size={15} color={blue_3} />
+        <span>{item}</span>
+      </p>
+    ))}
+  </ul>
+);
+
 const About = () => {
   const {
     title,
@@ -34,31 +74,10 @@ const About = () => {
       <PageTitle title={title} effect="blue" />
 
       {/* Photo and Socials Section */}
-      <div className="grid grid-cols-[40%,60%] items-center mb-5">
-        <div className="flex justify-center">
-          <Image
-            src={"/images/profile.png"}
-            width={200}
-            height={200}
-            alt={"Pradipta's Profile Photo"}
-          />
-        </div>
-        <div className="flex flex-col items-center">
-          <h1 className="text-black text-xl md:text-4xl font-poppins-bold text-center">
-            {story_title}
-          </h1>
-        </div>
-      </div>
+      <PhotoAndSocials story_title={story_title} />
 
       {/* Introduction */}
-      <div>
-        <p
-          className="info text-justify"
-          dangerouslySetInnerHTML={{
-            __html: `Greetings! I'm <span class="font-bold">${name}</span>, ${about}`,
-          }}
-        />
-      </div>
+      <Introduction name={name} about={about} />
 
       {/* Current Activities */}
       <SectionHeading title="📝 Now">
@@ -68,14 +87,7 @@ const About = () => {
       {/* Interests */}
       <SectionHeading title="🎈 What I'm Into">
         <p className="info text-justify">{interest.title}</p>
-        <ul>
-          {interest.list.map((item, index) => (
-            <p key={index} className="info text-justify my-2 flex gap-3">
-              <ChevronsRight size={15} color={blue_3} />
-              <span>{item}</span>
-            </p>
-          ))}
-        </ul>
+        <InterestsList interest={interest} />
       </SectionHeading>
 
       {/* Hobbies */}
