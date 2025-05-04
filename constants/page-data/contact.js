4d7f4d62bcd133e509533a63fecffa99cb563867
diptags <rd.pradipta@gmail.com>
2025-05-04 18:15:02 +0700

    Make email text shorter

--- constants/page-data/contact.js
+++ constants/page-data/contact.js
@@ -4,7 +4,7 @@ export const contactPageData = {
   title: "Let's Connect",
   intro: "Feel free to reach out if you'd like to explore new opportunities or collaborate on exciting projects. Whether you have an idea to share, insights to discuss, or just want to chat about the dynamic world of technology, I'm always open to connect. Send me a message through the form below or via social media, and let’s bring ideas to life together.",
   form_title: "You can also use this form 📧",
-  email_title: "E-mail: ",
+  email_title: "Email: ",
   alternative: {
     telegram: links.telegram,
     email: "pradipta.samiadji@gmail.com"
